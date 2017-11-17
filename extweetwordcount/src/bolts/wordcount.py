878691c791b1c37fe739299cda212e1349d029d2
root <root@ip-172-31-16-240.ec2.internal>
2017-11-17 00:22:10 +0000

    wordcount.py fix

--- extweetwordcount/src/bolts/wordcount.py
+++ extweetwordcount/src/bolts/wordcount.py
@@ -1,19 +1,20 @@
-
 from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
 
 import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+#connect to database
+conn = psycopg2.connect(database="tcount",user="postgres",host="localhost",port="5432")
+
 
 class WordCounter(Bolt):
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
 
-        #connect to database
-        self.conn = psycopg2.connect(database="tcount",user="postgres",host="localhost",port="5432")
+
 
     def process(self, tup):
         word = tup.values[0]
@@ -29,9 +30,14 @@ class WordCounter(Bolt):
         # Table name: Tweetwordcount
         # you need to create both the database and the table in advance.
         # create cursor and update tweetwordcount table with our increment words
-        self.cur = self.conn.cursor()
-        self.cur.execute("UPDATE tweetwordcount SET count=%s where word=%s", (self.counts[word], word))
-        self.conn.commit()
+        cur = conn.cursor()
+
+        if self.counts[word] == 0:
+            cur.execute("INSERT INTO tweetwordcount (word, count) VALUES (%s, %s)", (word, self.counts[word]))
+        else:
+            cur.execute("UPDATE tweetwordcount SET count=%s WHERE word=%s",(self.counts[word], word))
+            
+        conn.commit()
 
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
